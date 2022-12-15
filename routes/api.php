4be156cfed6ac07163b0ad1cfb1c86df989cdcff
Gyüre Árpád <kek@com.hu>
2022-12-15 12:07:32 +0100

    middleware

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(["middleware" => "auth:sanctum"], function() {
+    Route::post("/products", [ProductController::class, "store"]);
+    Route::put("/update/{id}", [ProductController::class, "update"]);
+    Route::delete("/delete/{id}", [ProductController::class, "delete"]);
+});
+
 Route::post("/register", [AuthController::class, "signUp"]);
 Route::post("/login", [AuthController::class, "signIn"]);
-Route::post("/products", [ProductController::class, "store"]);
 Route::get("/index", [ProductController::class, "index"]);
 Route::put("/show/{id}", [ProductController::class, "show"]);
-Route::put("/update/{id}", [ProductController::class, "update"]);
-Route::delete("/delete/{id}", [ProductController::class, "delete"]);
